13f1ead31d0e65e4871ca1ecd5b239b766903e01
Sohan Jain <sj346@cornell.edu>
2013-04-12 12:30:11 -0400

    get salt from settings file

--- tempus/utils.py
+++ tempus/utils.py
@@ -1,11 +1,17 @@
 from django.core import signing
+from django.conf import settings
 
 
 def tempus_dumps(data, salt='tempus'):
+    if hasattr(settings, 'TEMPUS_SALT'):
+        salt = settings.TEMPUS_SALT
+
     encrypted_data = signing.dumps(data, salt=salt)
     return encrypted_data
 
 
 def tempus_loads(encrypted_data, salt='tempus', max_age=None):
+    if hasattr(settings, 'TEMPUS_SALT'):
+        salt = settings.TEMPUS_SALT
     data = signing.loads(encrypted_data, salt=salt, max_age=max_age)
     return data
